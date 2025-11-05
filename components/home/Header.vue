a3cb85e16150a87780646b37f9c754fcd8c37c7a
Vishnu_Mac <thenormvg@gmail.com>
2025-11-05 20:50:35 +0530

    feat(navbar): Add CLI link and re-enable APP link
    Adds a link to the CLI and re-enables the APP link.

--- components/home/Header.vue
+++ components/home/Header.vue
@@ -20,9 +20,13 @@ import { useInView, motion } from "motion-v"
         :animate="{ opacity: 1, y: 0,  filter: 'blur(0px)' }"
         :transition="{  delay: 0.2 , duration:0.3}"
     >
-      <!-- <NuxtLink to="/app">
+       <NuxtLink to="https://github.com/TheAlphaOnes/cook">
+        <div class="nav-item">CLI</div>
+      </NuxtLink>
+
+      <NuxtLink to="/app">
       <div class="nav-item">APP</div>
-      </NuxtLink> -->
+      </NuxtLink>
 
       <div class="nav-item" @click="scrollToElement('#feature-section')">FEATURES</div>
 
